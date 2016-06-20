89257fb3fda251888c4880a7f446b3f12edc250e
Hugo <hingelmo@gmail.com>
2016-06-20 11:38:38 +0200

    update1

---
+++ app/src/main/java/com/deepred/subworld/utils/Fx.java
@@ -0,0 +1,62 @@
+package com.deepred.subworld.utils;
+
+import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.deepred.subworld.MyAdapter;
+import com.deepred.subworld.R;
+
+/**
+ * Created by Hugo on 6/17/2016.
+ */
+public class Fx {
+
+
+//...
+
+    /**
+     * @param ctx
+     * @param v
+     */
+    public static void slide_down(Context ctx, View v) {
+
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_down);
+        if (a != null) {
+            a.reset();
+            if (v != null) {
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+
+    public static void slide_up(Context ctx, View v, final MyAdapter.OnAnimationEndCompleteListener cb) {
+
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_up);
+        if (a != null) {
+            a.reset();
+            if (v != null) {
+                v.clearAnimation();
+                v.startAnimation(a);
+                a.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation arg0) {
+                        cb.onAnimationComplete();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+        }
+    }
+}
